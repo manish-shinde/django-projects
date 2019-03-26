7a61d8358133855004fa459bdbc5f80bc2d9a5a8
manish <manish.shinde69@gmail.com>
2019-03-26 15:02:47 +0530

    adding screenshots

--- weather_app/django2-env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!e:\full-scale-django-projects\weather_app\django2-env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
